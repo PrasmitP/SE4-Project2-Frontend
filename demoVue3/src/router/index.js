4a55db927fc3cca33062bfc501d643f65682de2c
PrasmitP <p.pokhrel@eagles.oc.edu>
2024-10-03 13:16:09 -0500

    Cleaned up code

--- demoVue3/src/router/index.js
+++ demoVue3/src/router/index.js
@@ -1,27 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import CourseList from '@/components/CourseList.vue'
 import UpdateCourse from '@/components/UpdateCourse.vue'
 import AddCourse from '@/components/AddCourse.vue'
-import EditCourse from '../components/EditCourse.vue'
-import about from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: about
-    },
     {
       path: '/CourseList',
       name: 'CourseList',
@@ -36,11 +20,6 @@ const router = createRouter({
       path: '/AddCourse',
       name: 'AddCourse',
       component: AddCourse
-    },
-    {
-      path: '/EditCourse',
-      name: 'EditCourse',
-      component: EditCourse
     }
   ]
 })
