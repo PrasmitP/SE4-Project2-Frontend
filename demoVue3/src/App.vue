6643f2caee670ebc6b6dacea4559281c17ee1361
dsantos-emps <david.santos@eagles.oc.edu>
2024-10-10 05:41:58 -0500

    added functionally to buttons
    now everything talks to the database

--- demoVue3/src/App.vue
+++ demoVue3/src/App.vue
@@ -2,94 +2,22 @@
   <div id="app">
     <h1>University Course Management</h1>
     <!-- Add Course Form -->
-    <add-course @addCourse="addCourse" />
+    <add-course/>
 
     <!-- Course List Table -->
-    <course-list 
-      :courses="courses" 
-      @editCourse="editCourse" 
-      @deleteCourse="deleteCourse" 
-    />
+    <course-list/>
 
-    <!-- Update Course Form -->
-    <update-course 
-      v-if="editingCourse" 
-      :course="editingCourse" 
-      @updateCourse="updateCourse" 
-      @cancelUpdate="cancelEditing" 
-    />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
 import AddCourse from './components/AddCourse.vue';
 import CourseList from './components/CourseList.vue';
-import UpdateCourse from './components/UpdateCourse.vue';
 
 export default {
   components: {
     AddCourse,
     CourseList,
-    UpdateCourse
   },
-  setup() {
-    const courses = ref([
-      {
-        id: 1, 
-        name: 'Introduction to Physics', 
-        department: 'PHYS', 
-        number: '0010', 
-        level: 1, 
-        hours: 3,
-        description: 'Basic principles of physics'
-      },
-      {
-        id: 2, 
-        name: 'Calculus I', 
-        department: 'MATH', 
-        number: '0020', 
-        level: 1, 
-        hours: 4,
-        description: 'Introduction to calculus concepts'
-      }
-    ]);
-
-    const editingCourse = ref(null); // Track the course being edited
-
-    const addCourse = (course) => {
-      courses.value.push({ ...course, id: Date.now() });
-    };
-
-    const editCourse = (course) => {
-      editingCourse.value = { ...course }; // Set the course for editing
-    };
-
-    const updateCourse = (updatedCourse) => {
-      const index = courses.value.findIndex(c => c.id === updatedCourse.id);
-      if (index !== -1) {
-        courses.value[index] = updatedCourse;
-      }
-      editingCourse.value = null; // Clear editing state after update
-    };
-
-    const cancelEditing = () => {
-      editingCourse.value = null; // Clear the course being edited
-    };
-
-    const deleteCourse = (id) => {
-      courses.value = courses.value.filter(course => course.id !== id);
-    };
-
-    return {
-      courses,
-      editingCourse,
-      addCourse,
-      editCourse,
-      updateCourse,
-      cancelEditing,
-      deleteCourse
-    };
-  }
 };
 </script>
--- demoVue3/src/components/AddCourse.vue
+++ demoVue3/src/components/AddCourse.vue
@@ -1,38 +1,37 @@
 <template>
-  <button v-if="!showForm" @click="toggleForm">Add Course</button>
-  <div v-if="showForm" class="overlay">
-    <div class="model">
-      <form v-if="showForm" @submit.prevent="submitCourse">
-        <div>
-          <label for="name">Course Name: </label>
-          <input class="textInput" v-model="course.name" type="text" id="name" required />
-        </div>
-        <div class="form-group">
-          <label for="department">Department:</label>
-          <input v-model="course.department" type="text" id="department" required maxlength="4" />
-        </div>
-        <div class="form-group">
-          <label for="number">Course Number: </label>
-          <input class="textInput" v-model="course.courseNumber" type="text" id="number" required maxlength="9" />
-        </div>
-        <div class="form-group">
-          <label for="level">Course Level:</label>
-          <input v-model="course.level" type="number" id="level" required min="1" max="9" />
-        </div>
-        <div class="form-group">
-          <label for="hours">Credit Hours:</label>
-          <input v-model="course.hours" type="number" id="hours" required min="1" max="9" />
-        </div>
-        <div class="form-group">
-          <label for="description">Description: </label>
-          <textarea class="descInput" v-model="course.description" type="text" id="description" />changes
-        </div>
-        <div class="form-actions">
-          <button type="submit">Confirm</button>
-          <button type="button" @click="cancelForm">Cancel</button>
-        </div>
-      </form>
-    </div>
+  <button id="AddButton" @click="toggleForm()">Add Course</button>
+  <div v-if="showForm" @click.self="cancelForm()" class="form-container">
+    <form @submit.prevent="submitCourse" class="course-form">
+      <h2>Add Course</h2>
+      <div class="form-group">
+        <label for="name">Course Name:</label>
+        <input v-model="course.name" type="text" id="name" required />
+      </div>
+      <div class="form-group">
+        <label for="department">Department:</label>
+        <input v-model="course.department" type="text" id="department" required maxlength="4" />
+      </div>
+      <div class="form-group">
+        <label for="number">Course Number:</label>
+        <input v-model="course.courseNumber" type="text" id="number" required maxlength="9" />
+      </div>
+      <div class="form-group">--
+        <label for="level">Course Level:</label>
+        <input v-model="course.level" type="number" id="level" required min="0" max="9" />
+      </div>
+      <div class="form-group">
+        <label for="hours">Credit Hours:</label>
+        <input v-model="course.hours" type="number" id="hours" required min="0" max="9" />
+      </div>
+      <div class="form-group">
+        <label for="description">Description:</label>
+        <input v-model="course.description" type="text" id="description" />
+      </div>
+      <div class="form-actions">
+        <button type="submit">Confirm</button>
+        <button type="button" @click="cancelForm">Cancel</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -53,14 +52,20 @@ export default {
         description: ''
       }
     };
-
-    // Handles the form submission
-    const submitCourse = () => {
-      emit('addCourse', { ...course.value });
-      course.value = {
+  },
+  methods: {
+    toggleForm() {
+      this.showForm = !this.showForm;
+    },
+    cancelForm() {
+      this.showForm = false;
+      this.resetCourse();
+    },
+    resetCourse() {
+      this.course = {
         name: '',
         department: '',
-        courseNumber: '',
+        number: '',
         level: null,
         hours: null,
         description: ''
@@ -75,13 +80,12 @@ export default {
         hours: this.course.hours,
         description: this.course.description,
       };
-      console.log(courseData);
+      console.log(courseData)
       CourseServices.create(courseData)
         .then((response) => {
           console.log("add " + response.data);
         })
         .catch((e) => {
-          // message.value = e.response.data.message;
           console.log(e);
         });
       this.showForm = false;
